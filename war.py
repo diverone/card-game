6863ea5d80614dc2637d2c3f9779c653e9f6080a
Simonas Gardauskas <simonas@gardauskas.lt>
2023-08-11 15:29:36 +0300

    project file war.py added

---
+++ war.py
@@ -0,0 +1,29 @@
+# Card game "War" Setup:
+
+# 1. Create the basic structures:
+#    - Create a Card class: Each card has a suit and rank.
+#    - Create a Deck class: The deck contains 52 cards, shuffled randomly.
+#    - Create a Player class: Each player has a name and a hand of cards.
+
+
+# 2. Shuffling and Dealing:
+#    - In the Deck class, implement methods to shuffle the cards and deal cards to players.
+
+
+# 3. Game Logic:
+#    - In the Player class, implement methods to draw a card from their hand and compare cards with another player.
+#    - In the main game loop, players take turns drawing cards, comparing ranks, and resolving "war" scenarios.
+
+
+# 4. Winning:
+#    - Keep track of the number of cards each player has. The player with all the cards wins.
+
+
+# 5. User Interaction:
+#    - Display game prompts to the user for each round.
+#    - Show the cards played by both players and the outcome of each round.
+#    - Display the winner at the end of the game.
+
+
+# 6. Error Handling:
+#    - Handle cases where players run out of cards, especially during "war" scenarios.
